93bdc29600db60d52dd07174043d19966ff7b109
SebastianPR890 <sebitasperirodri850@gmail.com>
2025-10-01 10:10:44 -0500

    se añadio la base de datos de aws

--- platzi_api_store/platzi_api_store/settings.py
+++ platzi_api_store/platzi_api_store/settings.py
@@ -184,9 +184,13 @@ WSGI_APPLICATION = "platzi_api_store.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'masterpassword',
+        'HOST': 'db-platzi-store.cpq0museykin.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
